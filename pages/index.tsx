e7d578fcde965b7deebb20efd136a7e7abd6848b
stephanieyen <steph.yensy@gmail.com>
2022-07-06 20:16:40 -0400

    functionality: add multiple cities (dummy data)

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ export default function Index() {
         main: {temp: number},
         weather: {description: string}[],
     } | null>(null);
+    const [newLocation, setNewLocation] = useState("");
+    const [locations, setLocations] = useState<string[]>([]);
 
     const [search, setSearch] = useState("");
 
@@ -24,6 +26,11 @@ export default function Index() {
         });
     }, []);
 
+    function onAdd() {
+        setLocations([...locations, newLocation]);
+        setNewLocation("");
+    }
+
     function onSearch() {
         axios.get("https://api.openweathermap.org/data/2.5/weather", {
             params: {
@@ -38,28 +45,44 @@ export default function Index() {
     }
 
     return (
-        <>
-            <div className="px-4 mx-auto max-w-lg">
-                <div className="flex items-center py-4">
-                    <p className="font-bold">Weather App</p>
-                    <input className="ml-auto border border-black text-sm p-1 mr-2"
-                        type="text" placeholder="Add new location..."/>
-                    <button className="px-2 py-1 text-sm bg-black text-white">Add</button>
-                </div>
+        <div className="px-4 mx-auto max-w-lg">
+
+            <div className="flex items-center py-4">
+                <p className="font-bold">Weather App</p>
+                <input className="ml-auto border border-black text-sm p-1 mr-2"
+                    type="text" placeholder="Add new location..." value={newLocation}
+                    onChange={e => setNewLocation(e.target.value)}/>
+                <button className="px-2 py-1 text-sm bg-black text-white" onClick={onAdd}>Add</button>
             </div>
 
-            <input type="text" value={search} onChange={e => 
-                setSearch(e.target.value)}/>
-            <button onClick={onSearch}>Search</button>
+            {locations.map(d => (
+                <div className="p-4 border border-black flex items-center mb-4">
+                    <div className="text-xl">
+                        <p className="font-bold">{d}</p>
+                        <p>77° F</p>
+                    </div>
+                    <div className="ml-auto text-right">
+                        <p> Feels like 80° F / Mostly cloudy</p>
+                        <p>Min 70° F / Max 85° F</p>
+                    </div>
+                </div>
+            ))}
 
-            {weather ? (
-                <>
-                    <p>{Math.round((weather.main.temp - 276) * 9 / 5 + 32)} deg F</p>
-                    <p>{weather.weather[0].description}</p>
-                </>
-            ) : (
-                <p>Loading</p>
-            )}
-        </>
+        </div>
+
+        // <>
+        //     <input type="text" value={search} onChange={e => 
+        //         setSearch(e.target.value)}/>
+        //     <button onClick={onSearch}>Search</button>
+
+        //     {weather ? (
+        //         <>
+        //             <p>{Math.round((weather.main.temp - 276) * 9 / 5 + 32)} deg F</p>
+        //             <p>{weather.weather[0].description}</p>
+        //         </>
+        //     ) : (
+        //         <p>Loading</p>
+        //     )}
+        // </>
     );
 }
